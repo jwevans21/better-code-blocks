156a035e2c3712cbc3c53f4389765758b06c4e2d
Jacob Evans <jacob@jwevans.dev>
2022-06-29 01:18:06 -0700

    Update init functions
    Signed-off-by: Jacob Evans <jacob@jwevans.dev>

--- browser/chartjs.js
+++ browser/chartjs.js
@@ -1,12 +1,16 @@
-const charts = document.querySelectorAll('.chartjs');
+function chartInit() {
+   const charts = document.querySelectorAll('.chartjs');
 
-for (const chart of charts) {
-   const ctx = chart.getContext('2d');
+   for (const chart of charts) {
+      const ctx = chart.getContext('2d');
 
-   const data = JSON.parse(chart.textContent);
+      const data = JSON.parse(chart.textContent);
 
-   data.plugins = []; // to prevent execution of plugins that are potentially unsafe
+      data.plugins = []; // to prevent execution of plugins that are potentially unsafe
 
-   const chartjs = new Chart(ctx, data);
-   chartjs.render();
+      const chartjs = new Chart(ctx, data);
+      chartjs.render();
+   }
 }
+
+window.addEventListener('load', chartInit);
--- browser/mermaid.js
+++ browser/mermaid.js
@@ -4,13 +4,18 @@ mermaid.initialize({
    theme: 'forest',
 });
 
-const mermaids = document.querySelectorAll('.mermaid');
+function mermaidInit() {
+   const mermaids = document.querySelectorAll('.mermaid');
 
-for (const mermaidEl of mermaids) {
-   mermaidEl.getElementsByClassName('mermaid-svg')[0].innerHTML =
-      mermaid.render(
-         `mermaid-${mermaidEl.id}-final`,
-         mermaidEl.getElementsByClassName('mermaid-code')[0]?.textContent || ''
-      );
-   mermaidEl.classList.add('mermaid-rendered');
+   for (const mermaidEl of mermaids) {
+      mermaidEl.getElementsByClassName('mermaid-svg')[0].innerHTML =
+         mermaid.render(
+            `mermaid-${mermaidEl.id}-final`,
+            mermaidEl.getElementsByClassName('mermaid-code')[0]?.textContent ||
+               ''
+         );
+      mermaidEl.classList.add('mermaid-rendered');
+   }
 }
+
+document.body.addEventListener("load", mermaidInit);
